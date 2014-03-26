634faf4673c7d02bdecf4f8cae9330dbe54f83ee
ankhzet <ankhzet@gmail.com>
2014-03-26 11:04:56 +0200

    Initial Commit

---
+++ Sudoku/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  Sudoku
+//
+//  Created by Ankh on 26.03.14.
+//  Copyright (c) 2014 Ankh. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+	// Insert code here to initialize your application
+}
+
+@end
---
+++ Sudoku/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Sudoku
+//
+//  Created by Ankh on 26.03.14.
+//  Copyright (c) 2014 Ankh. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+	return NSApplicationMain(argc, argv);
+}
